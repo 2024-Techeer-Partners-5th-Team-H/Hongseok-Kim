bdc1252ed1452a04afbe8fa39969648e435cf6d8
김홍석 <drghdtjr@naver.com>
2024-11-14 19:53:32 +0900

    Revert "PostMapping"
    This reverts commit 07bd352a1de27c9adf52314450efe6869dd8a7af.

--- src/main/java/techeerpartners/techeerpartners/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package techeerpartners.techeerpartners.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import techeerpartners.techeerpartners.board.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
